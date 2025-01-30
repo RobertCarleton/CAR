f8ad71712f8406920d3a273050549b5c84b13361
Robert Carleton <123363877+RobertCarleton@users.noreply.github.com>
2025-01-30 14:18:59 +0000

    Week2P2

--- Week2/Assets/Scripts/Modes/Main.cs
+++ Week2/Assets/Scripts/Modes/Main.cs
@@ -1,14 +1,32 @@
 using UnityEngine;
+using UnityEngine.XR.ARFoundation;
 
 public class Main : MonoBehaviour
 {
+    [SerializeField]
+    ARPlaneManager planeManager;
+    bool isToggledOn = true;
     private void OnEnable()
     {
-        
+        UIManager.Instance.ShowScreen("Main");
     }
 
     private void OnDisable()
     {
         
+    }
+
+    public void TogglePlanes()
+    {
+        //bool toggle = planeManager.planePrefab.activeSelf;
+
+        isToggledOn = !isToggledOn;
+
+        planeManager.planePrefab.SetActive(!isToggledOn);
+
+        foreach (ARPlane plane in planeManager.trackables)
+        {
+            plane.gameObject.SetActive(!isToggledOn);
+        }
     }
 }
